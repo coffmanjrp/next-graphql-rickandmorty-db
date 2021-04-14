b08fd7afb59e01c16a5fa29e7aa86bc7da21fe32
coffmanjrp <coffmanjrp@gmail.com>
2021-04-13 19:29:14 -0700

    Set a link for each card

--- components/CharacterCards.jsx
+++ components/CharacterCards.jsx
@@ -1,26 +1,31 @@
-import { Heading, Text, SimpleGrid, Flex, Image } from '@chakra-ui/react';
+import NextLink from 'next/link';
+import { Heading, Text, SimpleGrid, Flex, Image, Link } from '@chakra-ui/react';
 
 function CharacterCards({ characters }) {
   return (
     <SimpleGrid columns={[1, 2, 3]} spacing="40px">
       {characters.map((character) => (
-        <Flex
-          key={character.id}
-          justify="center"
-          flexDirection="column"
-          p="1.5rem"
-          color="inherit"
-          border="1px solid #eaeaea"
-          borderRadius="10px"
-          transition="color 0.15s ease, border-color 0.15s ease"
-        >
-          <Image src={character.image} w="fill" />
-          <Heading as="h4" align="center" size="md" my="0.5rem">
-            {character.name}
-          </Heading>
-          <Text align="center"> Origin: {character.origin.name}</Text>
-          <Text align="center"> Location: {character.location.name}</Text>
-        </Flex>
+        <NextLink href={`/character/${character.id}`} passHref>
+          <Link>
+            <Flex
+              key={character.id}
+              justify="center"
+              flexDirection="column"
+              p="1.5rem"
+              color="inherit"
+              border="1px solid #eaeaea"
+              borderRadius="10px"
+              transition="color 0.15s ease, border-color 0.15s ease"
+            >
+              <Image src={character.image} w="fill" />
+              <Heading as="h4" align="center" size="md" my="0.5rem">
+                {character.name}
+              </Heading>
+              <Text align="center"> Origin: {character.origin.name}</Text>
+              <Text align="center"> Location: {character.location.name}</Text>
+            </Flex>
+          </Link>
+        </NextLink>
       ))}
     </SimpleGrid>
   );
